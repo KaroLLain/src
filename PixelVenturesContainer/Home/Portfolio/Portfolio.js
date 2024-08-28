e14bfc6cb1bb24e7bf7175bfe95153cd45ecc693
KaroLLain <karolina.anna.knap@gmail.com>
2024-08-28 09:35:44 +0200

    commit

--- PixelVenturesContainer/Home/Portfolio/Portfolio.js
+++ PixelVenturesContainer/Home/Portfolio/Portfolio.js
@@ -69,7 +69,7 @@ export default function Portfolio() {
             <div className="faIcons">
               <a
                 className="cardLink"
-                href="https://github.com/KaroLLain/praesterno"
+                href="https://github.com/KaroLLain/root-ecommerce"
               >
                 <i className="faIcons__item fa fa-github" aria-hidden="true" />
               </a>
