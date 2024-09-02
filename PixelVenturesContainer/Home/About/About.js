4eb624b305327bb61b64013df681195f731c6197
KaroLLain <karolina.anna.knap@gmail.com>
2024-09-02 12:39:08 +0200

    s

--- PixelVenturesContainer/Home/About/About.js
+++ PixelVenturesContainer/Home/About/About.js
@@ -1,59 +1,61 @@
-import React from "react";
-import profilePhoto from "../../../assets/img/portretkopia.jpg";
-import profilePhotoRound from "../../../assets/img/portretkopiaround.png";
-import { useInView } from 'react-intersection-observer';
-import './About.css';
+import React from 'react'
+import profilePhoto from '../../../assets/img/portretkopia.jpg'
+import profilePhotoRound from '../../../assets/img/portretkopiaround.png'
+import { useInView } from 'react-intersection-observer'
+import './About.css'
 
-export default function About() {
-  const { ref: myRef, inView: fadeInSection} = useInView();
+export default function About () {
+  const { ref: myRef, inView: fadeInSection } = useInView()
 
   return (
-    <section ref={myRef} className="about fade-in" id="aboutSection">
+    <section ref={myRef} className='about fade-in' id='aboutSection'>
+      <picture
+        className={`${'aboutPictureWrapper'} ${fadeInSection ? 'show' : ''}`}
+      >
+        <source media='(min-width: 891px)' srcSet={profilePhoto} />
+        <img src={profilePhotoRound} alt='' className='aboutProfilePhoto' />
+      </picture>
 
-      <picture className={`${"aboutPictureWrapper"} ${fadeInSection ? "show" : ''}`} >
-        <source media="(min-width: 891px)" srcSet={profilePhoto}/>
-          <img src={profilePhotoRound} alt="" className="aboutProfilePhoto" />
-        </picture>
-        
-  
-        <div className={`${"aboutBox"} ${fadeInSection ? "show" : ''}`}>
-          <h3 className="aboutHeader"><span className="muscle">Hello</span> I'm Karolina</h3>
-          <p className="aboutText">
-            It's been quite a journey since I first delved into the world of
-            coding in March 2022. At the beginning of this venture, I learned the
-            basics of frontend HTML, CSS, SASS, so that I could later
-            dive into technologies like JavaScript, React, or Typescript.{" "}
-            <br /> Driven by my love for creativity and willing to constantly
-            evolve I made creating websites my new passion. My aesthetic
-            sense and basic knowledge in UX/UI allows me to design sites, that are
-            modern, intuitive and user-friendly.
-            <br /> My goal is to build my knowledge and constantly learn new
-            technologies, to create exellent web applications, which are not only
-            beautiful, but also fully responsive and optimized. <br />
-            <br />I am open to both remote and on-site work.
-          </p>
-  
-          <div className="linksWrapper">
-            <div className="svgWrapper">
-              <svg height="40" width="150">
-                <rect className="shape" height="40" width="150" />
-              </svg>
-              <div className="text">About me</div>
-            </div>
-  
-            <div className="faWrapper">
-              <a className="faWrapperItem" href="https://github.com/KaroLLain">
-                <i className="fa fa-github-square" />
-              </a>
-              <a
-                className="faWrapperItem"
-                href="https://www.linkedin.com/in/karolina-knap-parzychowska-65b4a112b/"
-              >
-                <i className="fa fa-linkedin-square" />
-              </a>
-            </div>
+      <div className={`${'aboutBox'} ${fadeInSection ? 'show' : ''}`}>
+        <h3 className='aboutHeader'>
+          <span className='muscle'>Hello</span> I'm Karolina
+        </h3>
+        <p className='aboutText'>
+          It's been quite a journey since I first delved into the world of
+          coding in March 2022. At the beginning of this venture, I learned the
+          basics of frontend HTML, CSS, SASS, so that I could later dive into
+          technologies like JavaScript, React, or Typescript. <br /> Driven by
+          my love for creativity and willing to constantly evolve I made
+          creating websites my new passion. My aesthetic sense and basic
+          knowledge in UX/UI allows me to design sites, that are modern,
+          intuitive and user-friendly.
+          <br /> My goal is to build my knowledge and constantly learn new
+          technologies, to create exellent web applications, which are not only
+          beautiful, but also fully responsive and optimized. <br />
+          <br />I am open to both remote and on-site work.
+        </p>
+
+        <div className='linksWrapper'>
+          <div className='svgWrapper'>
+            <svg height='40' width='150'>
+              <rect className='shape' height='40' width='150' />
+            </svg>
+            <div className='text'>About me</div>
+          </div>
+
+          <div className='faWrapper'>
+            <a className='faWrapperItem' href='https://github.com/KaroLLain'>
+              <i className='fa fa-github-square' />
+            </a>
+            <a
+              className='faWrapperItem'
+              href='https://www.linkedin.com/in/karolina-knap-parzychowska-65b4a112b/'
+            >
+              <i className='fa fa-linkedin-square' />
+            </a>
           </div>
         </div>
+      </div>
     </section>
-  );
+  )
 }
--- PixelVenturesContainer/Home/Footer/Footer.js
+++ PixelVenturesContainer/Home/Footer/Footer.js
@@ -1,26 +1,26 @@
-import React from "react";
+import React from 'react'
 import './Footer.css'
 
-export default function Footer() {
+export default function Footer () {
   return (
-    <footer className="footer">
-      <div className="footerWrapper">
-        <p className="footerText">
-          CopyRight © <span className="footer__date">2024</span> KaroLain | All
-          Rights Reserved 
+    <footer className='footer'>
+      <div className='footerWrapper'>
+        <p className='footerText'>
+          CopyRight © <span className='footer__date'>2024</span> KaroLain | All
+          Rights Reserved
         </p>
-        <div className="faWrapper">
-            <a className="faWrapperItem" href="https://github.com/KaroLLain">
-              <i className="fa fa-github-square" />
-            </a>
-            <a
-              className="faWrapperItem"
-              href="https://www.linkedin.com/in/karolina-knap-parzychowska-65b4a112b/"
-            >
-              <i className="fa fa-linkedin-square" />
-            </a>
-          </div>
+        <div className='faWrapper'>
+          <a className='faWrapperItem' href='https://github.com/KaroLLain'>
+            <i className='fa fa-github-square' />
+          </a>
+          <a
+            className='faWrapperItem'
+            href='https://www.linkedin.com/in/karolina-knap-parzychowska-65b4a112b/'
+          >
+            <i className='fa fa-linkedin-square' />
+          </a>
+        </div>
       </div>
     </footer>
-  );
+  )
 }
